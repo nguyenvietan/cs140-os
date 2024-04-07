1e537d74a8396e19ca4e0a0a977fe71e009798c8
annv <annv@coccoc.com>
2024-04-07 15:57:04 +0700

    attempted to solve caltrain problem

--- cs140-ps0/caltrain.c
+++ cs140-ps0/caltrain.c
@@ -1,29 +1,68 @@
 #include "pintos_thread.h"
 
 struct station {
-	// FILL ME IN
+	struct condition *cond_train_arrived;
+	struct condition *cond_all_passengers_seated;
+	struct lock *lck;
+	int available_seats;
+	int waiting_passengers;
 };
 
 void
 station_init(struct station *station)
 {
-	// FILL ME IN
+	station->cond_train_arrived = malloc(sizeof(struct condition));
+	station->cond_all_passengers_seated = malloc(sizeof(struct condition));
+	station->lck = malloc(sizeof(struct lock));
+	cond_init(station->cond_train_arrived);
+	cond_init(station->cond_all_passengers_seated);
+	lock_init(station->lck);
+	station->available_seats = 0;
+	station->waiting_passengers = 0;
 }
 
 void
 station_load_train(struct station *station, int count)
 {
-	// FILL ME IN
+	lock_acquire(station->lck);
+	station->available_seats = count;
+
+	while (station->available_seats > 0 && station->waiting_passengers > 0) {
+		cond_broadcast(station->cond_train_arrived, station->lck);
+		cond_wait(station->cond_all_passengers_seated, station->lck);
+	}
+	printf("station->available_seats = %d, station->waiting_passengers = %d\n",
+		station->available_seats, station->waiting_passengers);
+
+	station->available_seats = 0;
+
+	lock_release(station->lck);
 }
 
 void
 station_wait_for_train(struct station *station)
 {
-	// FILL ME IN
+	lock_acquire(station->lck);
+	station->waiting_passengers++;
+
+	while (station->available_seats == 0) {
+		cond_wait(station->cond_train_arrived, station->lck);
+	}
+
+	station->waiting_passengers--;
+	station->available_seats--;
+
+	lock_release(station->lck);
 }
 
 void
 station_on_board(struct station *station)
 {
-	// FILL ME IN
+	lock_acquire(station->lck);
+
+	if (station->available_seats == 0 || station->waiting_passengers == 0) {
+		cond_signal(station->cond_all_passengers_seated, station->lck);
+	}
+
+	lock_release(station->lck);
 }
